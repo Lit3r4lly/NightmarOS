cce40d06a74d58a9332cc0c5ce396e43d9089730
Lit3r4lly <literallynotf@gmail.com>
2021-08-25 17:52:43 +0300

    Kernel::CR - updated some cr fixes

--- kernel/include/arch/i386/gdt/gdt.h
+++ kernel/include/arch/i386/gdt/gdt.h
@@ -10,9 +10,9 @@
 
 namespace GDT {
     // setup an memory flat model (32 bit registers can address all the address range)
-    constexpr uint kNumOfEntries = 5; // number of segment entries (descriptors)
-    constexpr u32int kLimit = 0xFFFFFFFF; // every segment limit
-    constexpr u32int kBaseAddress = 0x0; // every segment base address
+    constexpr uint kNumOfEntries = 5;
+    constexpr u32int kLimit = 0xFFFFFFFF;
+    constexpr u32int kBaseAddress = 0x0;
 
     // set 0x0C byte for the granularity_flags (see intel's manual for more details)
     constexpr u8int kGranularityFlags = GDT::DescriptorFLags::kNonAvlBit |
@@ -21,22 +21,22 @@ namespace GDT {
                                         GDT::DescriptorFLags::kPagesBit;
 
     struct GDTPointer {
-        u16int limit : 16; // the size of the table - 1
-        u32int base_address : 32; // the base address of the GDT, in other words - the first descriptor of the array
-    } PACKED; // packed - compiler should keep the types and not align them
+        u16int limit : 16;
+        u32int base_address : 32;
+    } PACKED;
     using GDTPointer = struct GDTPointer;
 
     /* See in the end of the document, the SegmentDescriptor structure by the intel's manual */
     // Each segment descriptor is 8-byte length
     struct SegmentDescriptor {
-        u16int limit_low : 16; // the lowest 2 bytes of the limit
-        u16int base_low : 16; // the lowest 2 bytes of the base address
-        u8int base_middle : 8; // the byte in the middle of the base address
-        u8int access_byte : 8; // the access byte includes some information
-        u8int limit_high : 4; // the highest 4 bits of the limit
-        u8int granularity_flags : 4; // 4 bits of some granularity_flags
-        u8int base_high : 8; // the highest byte of the base address
-    } PACKED; // packed - compiler should keep the types and not align them
+        u16int limit_low : 16;
+        u16int base_low : 16;
+        u8int base_middle : 8;
+        u8int access_byte : 8;
+        u8int limit_high : 4;
+        u8int granularity_flags : 4;
+        u8int base_high : 8;
+    } PACKED;
     using SegmentDescriptor = struct SegmentDescriptor;
 
     void Initialize(); // initialize the GDT, by creating kNumOfEntries segment descriptors
--- kernel/include/arch/i386/tty/tty.h
+++ kernel/include/arch/i386/tty/tty.h
@@ -27,8 +27,8 @@ enum class VgaColor {
     kVgaColorWhite
 };
 
-constexpr size_t kVGA_WIDTH = 80; // line fit 80 characters
-constexpr size_t kVGA_HEIGHT = 25; // 25 lines in the terminal
+constexpr size_t kVgaWidth = 80; // line fit 80 characters
+constexpr size_t kVgaHeight = 25; // 25 lines in the terminal
 uint16_t* const kTerminalBuffer = (uint16_t*)0xB8000; // buffer location
 
 static size_t terminal_row; // current cursor row location
@@ -38,10 +38,10 @@ static uint8_t terminal_color; // whole terminal color
 namespace TTY {
     void Initialize(); // Initialize terminal screen (Clear and set colors)
     void Clear(); // Clear terminal screen (set all of the cells to ' ')
-    inline uint8_t VgaEntryColor(enum VgaColor fg, enum VgaColor bg); // translate the color of the foreground and the background to an uint8_t entry usable for the VGA buffer
-    inline uint16_t VgaEntry(uchar uc, uint8_t color); // translate the color and the char to uint16_t entry usable for the buffer
-    void PutEntryAt(char c, uint8_t color, size_t x, size_t y); // put char with specific color in (x,y) coordinates
-    void PutEntryAt(uint16_t entry, size_t x, size_t y); // overloading for entry paramter
+    inline u8int VgaEntryColor(enum VgaColor fg, enum VgaColor bg); // translate the color of the foreground and the background to an uint8_t entry usable for the VGA buffer
+    inline u16int VgaEntry(uchar uc, u8int color); // translate the color and the char to uint16_t entry usable for the buffer
+    void PutEntryAt(char c, u8int color, size_t x, size_t y); // put char with specific color in (x,y) coordinates
+    void PutEntryAt(u16int entry, size_t x, size_t y); // overloading for entry paramter
     void PutChar(char c); // put char in current courser location
     void Write(const char* data, size_t size); // write string with size to the terminal
     void WriteString(const char* data); // write string without size to the terminal
