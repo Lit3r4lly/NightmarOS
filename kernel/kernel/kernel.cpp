fa1f7194ca3a7bb1f9b1377e5dccadafe15b6d25
Lit3r4lly <literallynotf@gmail.com>
2021-08-26 00:34:30 +0300

    Kernel: updated no_return attribute in kernel_main

--- kernel/kernel/kernel.cpp
+++ kernel/kernel/kernel.cpp
@@ -12,7 +12,7 @@
  * Entry point for the run of the kernel
  * In this point there some initialization of the components that necessary for running the system.
  */
-C_SCOPE void kernel_main() {
+C_SCOPE NO_RETURN void kernel_main() {
     TTY::Initialize();
     GDT::Initialize();
     TTY::WriteString("Hello World!\nJust entered to NightmareOS kernel!\n");
