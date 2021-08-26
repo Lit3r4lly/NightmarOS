52c9233d202e13d1a34189065e5c9f0ae6af489d
Lit3r4lly <literallynotf@gmail.com>
2021-08-26 19:36:20 +0300

    Scripts: updated qemu command-line arguments

--- scripts/run.sh
+++ scripts/run.sh
@@ -6,5 +6,20 @@ set -e # stops if there is error
 # strip symbols from binary
 objcopy --strip-debug "sysroot/boot/$PROJECT_NAME.kernel" "$PROJECT_NAME.sym"
 
+QEMU_ARGS=(
+    # Disable default devices, QEMU by default enables a ton of devices which slow down boot.
+    "-nodefaults"
+
+    "-vga"
+    "std" # Use a standard VGA for graphics
+
+    "-no-reboot" # exit instead of rebooting
+
+    # using this, the program can write to x86 I/O port 0xE9 and talk
+    "-debugcon"
+    "file:info.log" # if needed - change this file to "/dev/stdout", and the log will be written to file
+)
+
 # runs the iso file with qemu
-qemu-system-"$(./scripts/target-triplet-to-arch.sh "$HOST")" -cdrom "$PROJECT_NAME.iso" -debugcon file:info.log
+echo "Executing: qemu-system-i386 " "${QEMU_ARGS[@]}"
+qemu-system-"$(./scripts/target-triplet-to-arch.sh "$HOST")" -cdrom "$PROJECT_NAME.iso" "${QEMU_ARGS[@]}"
