4ce1e8897893cfc7ec456a5ddb870d9291ccad2e
Lit3r4lly <literallynotf@gmail.com>
2021-08-26 18:19:33 +0300

    Kernel: updated macro for logger

--- kernel/include/kernel/logs/log.h
+++ kernel/include/kernel/logs/log.h
@@ -10,15 +10,28 @@
 
 #include <arch/i386/ports/ports.h>
 
-#ifdef _K_LOG // if defined - log will be created
-    #define K_LOG(data) \
-        Log::Write(data)
-#else // if not defined - log will not be created
-    #define K_LOG(data)
-#endif
+// two macros defined for stringize some macros like __LINE__
+#define STRINGIZE_DETAIL(x) #x
+#define STRINGIZE(x) STRINGIZE_DETAIL(x)
 
 namespace Log {
     constexpr u16int kHackPort = 0xE9; // special "hack port" used in qemu and bochs for debugging and logging
 
     void Write(const char* data); // writes string to log output (stdout / file)
 };
+
+#define INIT "Initialization"
+#define SUCCESS "Success"
+#define ERROR "Error"
+#define PANIC "Panic"
+
+#ifdef _K_LOG // if defined - log will be created
+    #define K_LOG(status, data) \
+        Log::Write("[" status " - "); \
+        Log::Write(__FILE__ "::"); \
+        Log::Write(STRINGIZE(__LINE__) "::"); \
+        Log::Write(__FUNCTION__); \
+        Log::Write("] - " data);
+#else // if not defined - log will not be created
+    #define K_LOG(status, data)
+#endif
