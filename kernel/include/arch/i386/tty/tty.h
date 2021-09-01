28343d40d9ccc2f7b5f3984b8eb4f4301a4007d7
Lit3r4lly <literallynotf@gmail.com>
2021-09-02 01:37:21 +0300

    Kernel: updated some shitty documentations

--- kernel/include/arch/i386/tty/tty.h
+++ kernel/include/arch/i386/tty/tty.h
@@ -43,11 +43,11 @@ namespace TTY {
     static u8int terminal_color; // whole terminal color
 
     void Initialize(); // Initialize terminal screen (Clear and set colors)
-    void Clear(); // Clear terminal screen (set all of the cells to ' ')
+    void Clear(); // Clear terminal screen (set all the cells to ' ')
     inline u8int VgaEntryColor(enum VgaColor fg, enum VgaColor bg); // translate the color of the foreground and the background to an uint8_t entry usable for the VGA buffer
     inline u16int VgaEntry(uchar uc, u8int color); // translate the color and the char to uint16_t entry usable for the buffer
     void PutEntryAt(char c, u8int color, u8int x, u8int y); // put char with specific color in (x,y) coordinates
-    void PutEntryAt(u16int entry, u8int x, u8int y); // overloading for entry paramter
+    void PutEntryAt(u16int entry, u8int x, u8int y); // overloading for entry parameter
     void PutChar(char c); // put char in current courser location
     void Write(const char* data, size_t size); // write string with size to the terminal
     void WriteString(const char* data); // write string without size to the terminal
