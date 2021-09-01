8d122364f6ce9ba3da896bd98fd0c7fdab754713
oshr22 <oshritaub@gmail.com>
2021-09-01 20:39:20 +0300

    libc: add documentation for the fmt functions

--- libc/include/wrappers/fmt/fmt.h
+++ libc/include/wrappers/fmt/fmt.h
@@ -15,9 +15,9 @@ using out_function_type = u8int(*)(u8int);
 C_SCOPE {
 #endif
 
-    u32int fmt_digits(int val, out_function_type out_fun);
-    u32int fmt(const char* fmt, va_list args, out_function_type out_fun);
-    u32int fmt_string(const char* ch, out_function_type out_fun);
+    u32int fmt_digits(int val, out_function_type out_fun); //function to format number digits
+    u32int fmt(const char* fmt, va_list args, out_function_type out_fun); //function to handle the formatting of arguments into the string
+    u32int fmt_string(const char* ch, out_function_type out_fun); //function to format a string
 
 #ifdef __cplusplus
 };
