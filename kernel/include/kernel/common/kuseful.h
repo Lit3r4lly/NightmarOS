6b1c0598d9c8f2cbfd5c6df0bde1d9f69bdce860
Lit3r4lly <literallynotf@gmail.com>
2021-09-02 23:23:51 +0300

    Kernel: updated assert macros (added !cpp assert)

--- kernel/include/kernel/common/kuseful.h
+++ kernel/include/kernel/common/kuseful.h
@@ -12,7 +12,12 @@
 
 #define K_PANIC(info) Panic::Panic(info, __FILE__, __FUNCTION__, __LINE__)
 
-#define assert(expr) \
-    (static_cast<bool>(expr) \
-    ? void (0) \
-    : K_PANIC(#expr))
+#ifdef __cplusplus
+#define assert(expr) (static_cast<bool>(expr) \
+                    ? void (0)                \
+                    : K_PANIC(#expr))
+#else
+#define assert(expr) ((expr \
+                     ? void (0)       \
+                     : K_PANIC(#expr))
+#endif
