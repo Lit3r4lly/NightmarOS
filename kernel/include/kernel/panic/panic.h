7b63df66e0d77addc13c130a5bdb2244df237a89
Lit3r4lly <literallynotf@gmail.com>
2021-09-02 22:53:41 +0300

    Kernel: added panic function

---
+++ kernel/include/kernel/panic/panic.h
@@ -0,0 +1,17 @@
+/**
+* @file panic.h
+* @author Ori Feldman
+* @brief declare kernel panic
+* @date 9/2/21
+* @copyright Copyright (c) 2021
+*/
+
+#pragma once
+
+#include <kernel/common/kdefines.h>
+#include <libc/stdio.h>
+
+namespace Panic {
+    // panic and get into infinite loop
+    NO_RETURN void volatile Panic(const char* info, const char* file_name, const char* function_name, const u32int line);
+};
