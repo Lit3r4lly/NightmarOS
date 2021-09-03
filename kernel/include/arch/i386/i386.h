9a55eb6ff40d4b024487ce847438da4a94acd154
Lit3r4lly <literallynotf@gmail.com>
2021-09-03 18:36:05 +0300

    Kernel: updated i386 header file

--- kernel/include/arch/i386/i386.h
+++ kernel/include/arch/i386/i386.h
@@ -11,17 +11,24 @@
 #include <kernel/common/kdefines.h>
 
 namespace I386 {
-    // Stack state structure for ISRs (built as FIFO)
+    /*
+     * stack state structure for ISRs (built as FIFO)
+    */
     struct StackState {
-        u32int error_code : 32;
-        u32int eip : 32;
-        u32int cs : 32;
-        u32int eflags : 32;
+        u32int interrupt_number : 32; // pushed manually interrupt number
+        u32int error_code : 32; // interrupts 8, 10, 11, 12, 13, 14, 17 are error-code interrupts
+        u32int eip : 32; // pushed eip value before interrupt occur
+        u32int cs : 32; // pushed cs selector value before interrupt occur
+        u32int eflags : 32; // pushed eflags value before interrupt occur
     } PACKED;
     using StackState = struct StackState;
 
-    // Cpu state before ISR is occurred
+    /*
+     * cpu state before ISR is occurred.
+     * contains all the registers values before the interrupt occur
+    */
     struct CPUState {
+        // pushad registers
         u32int eax : 32;
         u32int ecx : 32;
         u32int edx : 32;
@@ -30,6 +37,14 @@ namespace I386 {
         u32int ebp : 32;
         u32int esi : 32;
         u32int edi : 32;
+
+        // manually pushed segment selector
+        u16int cs : 16;
+        u16int ds : 16;
+        u16int es : 16;
+        u16int fs : 16;
+        u16int gs : 16;
+        u16int ss : 16;
     } PACKED;
     using CPUState = struct CPUState;
 };
