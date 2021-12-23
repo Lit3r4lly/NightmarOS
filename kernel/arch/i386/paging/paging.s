9de79ef68bf95914290e0d9b34ed7b920beff24f
oshri22 <oshritaub@gmail.com>
2021-12-23 19:23:26 +0200

    kernel: add load end enable paging

---
+++ kernel/arch/i386/paging/paging.s
@@ -0,0 +1,29 @@
+.intel_syntax noprefix
+
+.global load_page_directory
+.type load_page_directory, @function
+load_page_directory:
+    push ebp
+    mov ebp, esp
+
+    mov eax, [ebp + 8]
+    mov cr3, eax
+
+    mov esp, ebp
+    pop ebp
+    ret
+
+
+.global enable_paging
+.type enable_paging, @function
+enable_paging:
+    push ebp
+    mov ebp, esp
+
+    mov eax, cr0
+    or eax, 0x80000001
+    mov cr0, eax
+
+    mov esp, ebp
+    pop ebp
+    ret
