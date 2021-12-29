f89f60d48a6e991a06919ac28ac565d2f840d64c
oshri22 <oshritaub@gmail.com>
2021-12-29 23:18:38 +0200

    kernel: fixed problem with types

--- kernel/include/arch/i386/paging/memory_manager.h
+++ kernel/include/arch/i386/paging/memory_manager.h
@@ -9,7 +9,6 @@
 
 #include <kernel/common/kuseful.h>
 #include <kernel/logs/logs.h>
-#include <arch/i386/interrupts/pic/pic.h>
 #include <arch/i386/i386.h>
 #include <arch/i386/paging/paging.h>
 
@@ -20,7 +19,7 @@ namespace MemoryManager {
 
     constexpr uint32_t kNumFrames = 0x1000000 / 0x1000; //the number of frames
     extern uint32_t* kFrames; // the available frames
-    extern uint32_t* KernelDir; //the directory of the kernel
+    extern Paging::PageDirectory* KernelDir; //the directory of the kernel
     extern uint32_t kBaseAddress; //the current available address
 
     uint32_t AllocateMemory(uint32_t size, int aligned, uint32_t* physical_address);
