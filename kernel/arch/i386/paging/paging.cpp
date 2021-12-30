3c000bd938086e397c860d650a3e7ad14c61dfb9
oshri22 <oshritaub@gmail.com>
2021-12-30 20:19:11 +0200

    kernel: rename to make things clear

--- kernel/arch/i386/paging/paging.cpp
+++ kernel/arch/i386/paging/paging.cpp
@@ -9,14 +9,14 @@
 //#include <arch/i386/paging/paging.h>
 #include <arch/i386/paging/memory_manager.h>
 
-uint32_t* MemoryManager::kFrames; // the available frames
+uint32_t* MemoryManager::kHeap; // the available frames
 Paging::PageDirectory* MemoryManager::KernelDir; //the directory of the kernel
 uint32_t  MemoryManager::kBaseAddress;
 
 void Paging::Initialize() {
 
     //allocating heap
-    MemoryManager::kFrames = (uint32_t*)MemoryManager::AllocateMemory(MemoryManager::kNumFrames / 32, 1,0);
+    MemoryManager::kHeap = (uint32_t*)MemoryManager::AllocateMemory(MemoryManager::kNumFrames / 32, 1,0);
 
     //allocating the kernel memory
     MemoryManager::KernelDir = (PageDirectory*)MemoryManager::AllocateMemory(sizeof(PageDirectory),1,0 );
